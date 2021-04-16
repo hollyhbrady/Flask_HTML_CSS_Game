af8ac3d6c385e23728a239f7288f02d051a0969e
hollyhbrady <hollyhbrady@gmail.com>
2021-04-16 18:56:03 +0100

    play_game function working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,13 @@
 from flask import render_template
 from app import app
-from modules.game import players
+from modules import game
 from modules.player import *
+
+@app.route('/')
+def home():
+    return "Hello! Here are the rules: Rock wins over scissors - because rock smashes scissors. Scissors wins over paper - because scissors cut paper. Paper wins over rock - because paper covers rock. Same gesture is a tie. Are you ready to play?"
+
+@app.route('/<player1>/<player2>')
+def play_game(player1, player2):
+    return f"1, 2, 3! {game.play_game(player1, player2)}"
+
--- modules/game.py
+++ modules/game.py
@@ -1,23 +1,23 @@
 from modules.player import Player
 
 def play_game(player1, player2):
-    if player1.gesture == "rock" and player2.gesture == "scissors":
+    if player1 == "rock" and player2 == "scissors":
         return "Player 1 wins by playing rock"
-    if player1.gesture == "scissors" and player2.gesture == "rock":
+    if player1 == "scissors" and player2 == "rock":
         return "Player 2 wins by playing rock"
-    if player1.gesture == "scissors" and player2.gesture == "paper":
+    if player1 == "scissors" and player2 == "paper":
         return "Player 1 wins by playing scissors"
-    if player1.gesture == "paper" and player2.gesture == "scissors":
+    if player1 == "paper" and player2 == "scissors":
         return "Player 2 wins by playing scissors"
-    if player1.gesture == "rock" and player2.gesture == "paper":
-        return "Player 2 wins by playing paper"
-    if player1.gesture == "paper" and player2.gesture == "rock":
+    if player1 == "paper" and player2 == "rock":
         return "Player 1 wins by playing paper"
-    if player1.gesture == "rock" and player2.gesture == "rock":
-        return "No winner, this game is a draw"
-    if player1.gesture == "paper" and player2.gesture == "paper":
-        return "No winner, this game is a draw"
-    if player1.gesture == "scissors" and player2.gesture == "scissors":
-        return "No winner, this game is a draw"
+    if player1 == "rock" and player2 == "paper":
+        return "Player 2 wins by playing paper"
+    if player1 == "rock" and player2 == "rock":
+        return "No winner, this game is a draw. Best of 3?"
+    if player1 == "paper" and player2 == "paper":
+        return "No winner, this game is a draw. Best of 3?"
+    if player1 == "scissors" and player2 == "scissors":
+        return "No winner, this game is a draw, Best of 3?"
     else:
         return "What game are you trying to play?"
