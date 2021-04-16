4f5700a620ac14606f719864ab16d275445759e1
hollyhbrady <hollyhbrady@gmail.com>
2021-04-16 17:27:26 +0100

    Game tests pass

--- tests/game_tests.py
+++ tests/game_tests.py
@@ -5,6 +5,42 @@ from modules.player import Player
 class TestGame(unittest.TestCase):
 
     def setUp(self):
-        self.roosa = Player("Roosa", "rock")
-        self.colin = Player("Colin", "paper")
-        self.hannah = Player("Hannah", "scissors")
+        self.rock = Player("Roosa", "rock")
+        self.paper = Player("Colin", "paper")
+        self.scissors = Player("Hannah", "scissors")
+
+    def test_decide_rock_and_scissors(self):
+        result = play_game(self.rock, self.scissors)
+        self.assertEqual("Player 1 wins by playing rock", result)
+        
+    def test_decide_scissors_and_rock(self):
+        result = play_game(self.scissors, self.rock)
+        self.assertEqual("Player 2 wins by playing rock", result)
+        
+    def test_decide_paper_and_scissors(self):
+        result = play_game(self.paper, self.scissors)
+        self.assertEqual("Player 2 wins by playing scissors", result)
+
+    def test_decide_scissors_and_paper(self):
+        result = play_game(self.scissors, self.paper)
+        self.assertEqual("Player 1 wins by playing scissors", result)
+        
+    def test_decide_rock_and_paper(self):
+        result = play_game(self.rock, self.paper)
+        self.assertEqual("Player 2 wins by playing paper", result)
+
+    def test_decide_paper_and_rock(self):
+        result = play_game(self.paper, self.rock)
+        self.assertEqual("Player 1 wins by playing paper", result)
+
+    def test_decide_paper_draw(self):
+        result = play_game(self.paper, self.paper)
+        self.assertEqual("No winner, this game is a draw", result)
+
+    def test_decide_rock_draw(self):
+        result = play_game(self.rock, self.rock)
+        self.assertEqual("No winner, this game is a draw", result)
+        
+    def test_decide_scissors_draw(self):
+        result = play_game(self.scissors, self.scissors)
+        self.assertEqual("No winner, this game is a draw", result)    
--- tests/player_tests.py
+++ tests/player_tests.py
@@ -4,10 +4,10 @@ from modules.player import Player
 class TestPlayer(unittest.TestCase):
 
     def setUp(self):
-        self.roosa = Player("Roosa", "Rock")
+        self.roosa = Player("Roosa", "rock")
 
     def test_player_has_name(self):
         self.assertEqual("Roosa", self.roosa.name)
     
     def test_player_has_gesture(self):
-        self.assertEqual("Rock", self.roosa.gesture)
+        self.assertEqual("rock", self.roosa.gesture)
