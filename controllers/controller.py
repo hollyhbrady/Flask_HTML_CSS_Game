6c4575f0470d0e7d7a8b06b1c9ebc63c39da4f27
hollyhbrady <hollyhbrady@gmail.com>
2021-04-17 19:34:55 +0100

    base and home html pages working

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,9 +5,12 @@ from modules.player import *
 
 @app.route('/')
 def home():
-    return "Hello! Here are the rules: Rock wins over scissors - because rock smashes scissors. Scissors wins over paper - because scissors cut paper. Paper wins over rock - because paper covers rock. Same gesture is a tie. Are you ready to play?"
+    return render_template('home.html', title='Home')
 
 @app.route('/<player1>/<player2>')
 def play_game(player1, player2):
-    return f"1, 2, 3! {game.play_game(player1, player2)}"
+    return render_template('result.html', title='Game Result')
+    
+    
+    f"1, 2, 3! {game.play_game(player1, player2)}"
 
